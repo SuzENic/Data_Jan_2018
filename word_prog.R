817b31461a9c2ea915776f759cf6778afca06fbc
Suzanne Nichols <senichol@Suzannes-MacBook-Air.local>
2018-02-05 00:35:30 -0500

    more comments

--- word_prog.R
+++ word_prog.R
@@ -2,23 +2,24 @@ word_prog <- function(){
 	  library(devtools)
 	  library(rword2vec)
 	  setwd("/Users/senichol/Desktop/Data_Inc/Data_Jan_2018")
-	  data <- read.csv("hate_speech/data/labeled_data.csv")
-	  train=data$tweet
-	  train=tolower(train)
-	  train=gsub("[[:punct:]]", "", train)
-	  write(train,"text_data.txt")
+	  data <- read.csv("hate_speech/data/labeled_data.csv") # get the imput corpus
+	  train=data$tweet					# make the training input
+	  train=tolower(train)					# make everything lower case because he and He are the same.
+	  train=gsub("[[:punct:]]", "", train)			# punctuation will not be matched so take it out
+	  write(train,"text_data.txt")				#put training in a text file
+	  							# run word2vec and generate the model
 	  model=word2vec(train_file = "text_data.txt",output_file ="model1.bin",layer1_size = 300,min_count = 40,num_threads = 4,window = 10,sample = 0.001,binary=1)
-	  bin_to_txt("model1.bin","model1text.txt")
+	  bin_to_txt("model1.bin","model1text.txt")		  #output model to text file
 	  
-	  dist=distance(file_name = "model1.bin",search_word = "bitch",num = 10)
+	  dist=distance(file_name = "model1.bin",search_word = "bitch",num = 10)	#calculate nearest neighbors for 2 words
 	  dist2=distance(file_name = "model1.bin",search_word = "ass",num = 10)
 
-	  pdf(file="Plot1.pdf")	  #scatter plot of closeness
+	  pdf(file="Plot1.pdf")								#scatter plot of closeness
 	  plot(data$bitch, data$ass, na.rm=TRUE, xlim = c(.93, 1.01), ylim = c(.86, 1.02), xlab = "Bitch", ylab = "Ass", main = "Cos Distances Between Words")
 	  text(data$bitch, data$ass+.015, data$X.1)
 	  dev.off()
 
-	  pdf(file="Plot2.pdf") #cluster	  #auc, confusion matrix, precision recall curve t-sne
+	  pdf(file="Plot2.pdf")								#Cluster diagram  
 	  dist=distance(file_name = "model1.bin",search_word = "bitch",num = 40)
 	  hdist <- matrix(1:40, ncol = 1)
 	  rownames(hdist) <- dist$word
@@ -27,6 +28,11 @@ word_prog <- function(){
 	  plot(hc2, main = "Cluster Dendrogram for Bitch")
 	  dev.off()
 
-	  ## model=word2vec(train_file = "hate_speech/data/labeled_data.csv",output_file = "vec.bin",binary=0)
-	  ###/Library/Frameworks/R.framework
+	  ### this is where I will test what I have so far, look for similarities in tweets. 
+	  ### This is where I will test models to look for improvements in sematics. 
+	      	      	      ### They will include, but nto be limited to...
+			      ### logistic regression
+			      ### Principal Component Analysis
+			      ### Tensor Flow
+
 }
