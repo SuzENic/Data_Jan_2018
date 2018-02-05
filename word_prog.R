0d02129161ce308d81da1c72215246791c6d26d3
Suzanne Nichols <senichol@Suzannes-MacBook-Air.local>
2018-02-05 00:15:51 -0500

    Added plot2 and the code for same

--- word_prog.R
+++ word_prog.R
@@ -13,9 +13,18 @@ word_prog <- function(){
 	  dist=distance(file_name = "model1.bin",search_word = "bitch",num = 10)
 	  dist2=distance(file_name = "model1.bin",search_word = "ass",num = 10)
 
-	  pdf(file="Plot1.pdf")	  
-	  plot(data$bitch, data$ass, na.rm=TRUE, xlim = c(.93, 1.01), ylim = c(.86, 1.02), xlab = "Bitch", ylab = "Ass")
+	  pdf(file="Plot1.pdf")	  #scatter plot of closeness
+	  plot(data$bitch, data$ass, na.rm=TRUE, xlim = c(.93, 1.01), ylim = c(.86, 1.02), xlab = "Bitch", ylab = "Ass", main = "Cos Distances Between Words")
 	  text(data$bitch, data$ass+.015, data$X.1)
+	  dev.off()
+
+	  pdf(file="Plot2.pdf") #cluster	  #auc, confusion matrix, precision recall curve t-sne
+	  dist=distance(file_name = "model1.bin",search_word = "bitch",num = 40)
+	  hdist <- matrix(1:40, ncol = 1)
+	  rownames(hdist) <- dist$word
+	  hdist[,1] <- as.numeric(levels(dist[,2]))[dist[,2]]
+	  hc2 = hclust(dist(hdist))
+	  plot(hc2, main = "Cluster Dendrogram for Bitch")
 	  dev.off()
 
 	  ## model=word2vec(train_file = "hate_speech/data/labeled_data.csv",output_file = "vec.bin",binary=0)
